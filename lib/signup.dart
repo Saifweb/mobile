09a4e7b9c71d20424f04ffb1257c7f04dd04d34b
Saifweb <seifeddine.benhamida@supcom.tn>
2023-01-16 01:05:37 +0100

    fix problem

--- lib/signup.dart
+++ lib/signup.dart
@@ -14,162 +14,94 @@ import 'package:mobile_project/dashbord.dart';
 
 import 'globals.dart' as globals;
 
-
-
 var currentStep = 0;
 
 var items = [
-
   'housekeeper',
-
   'customer',
-
 ];
 
 String dropdownvalue = 'housekeeper';
 
 Future<User> createUser(
-
-    BuildContext context,
-
-    String name,
-
-    String email,
-
-    String state,
-
-    String phoneNumber,
-
-    String location,
-
-    String password,
-
-    String confirmpassword,
-
-    String age,
-
-    ) async {
-
+  BuildContext context,
+  String name,
+  String email,
+  String state,
+  String phoneNumber,
+  String location,
+  String password,
+  String confirmpassword,
+  String age,
+) async {
   if (password != confirmpassword) {
-
-    throw Exception('Wrong password');
-
+    print("hello");
+    return User(email: "", password: "", name: "", state: "");
   } else {
-
     try {
-
       final response = await http.post(
-
         Uri.parse('https://mobilebackend.onrender.com/api/signup'),
-
         headers: <String, String>{
-
           'Content-Type': 'application/json; charset=UTF-8',
-
         },
-
         body: jsonEncode(<String, String>{
-
           'name': name,
-
           'email': email,
-
           'state': state,
-
           'location': location,
-
           'phoneNumber': phoneNumber,
-
           'password': password,
-
           'age': age,
-
         }),
-
       );
 
       if (response.statusCode == 200) {
-
         // If the server did return a 200 OK response,
 
         // then parse the JSON.
 
         // return User.fromJson(jsonDecode(response.body));
-
         Navigator.push(
-
             context, MaterialPageRoute(builder: (context) => Dashbord()));
 
         return User(email: "", password: "", name: "", state: "");
-
       } else {
-
         // If the server did not return a 201 CREATED response,
 
         // then throw an exception.
 
-        throw Exception(response.body);
-
+        return User(email: "", password: "", name: "", state: "");
       }
-
     } catch (e) {
-
       showDialog(
-
         context: context,
-
         builder: (context) {
-
           return AlertDialog(
-
             title: Text('Error'),
-
             content: Text(
-
               e.toString(),
-
               style: TextStyle(
-
                 color: Colors.red,
-
               ),
-
             ),
-
             actions: <Widget>[
-
               TextButton(
-
                 child: Text('Close'),
-
                 onPressed: () {
-
                   Navigator.of(context).pop();
-
                 },
-
               ),
-
             ],
-
           );
-
         },
-
       );
 
       return User(email: "", password: "", name: "", state: "");
-
     }
-
   }
-
 }
 
-
-
 class User {
-
   final String email;
 
   final String password;
@@ -178,52 +110,28 @@ class User {
 
   final String state;
 
-
-
   const User(
-
       {required this.email,
-
-        required this.password,
-
-        required this.name,
-
-        required this.state});
-
-
+      required this.password,
+      required this.name,
+      required this.state});
 
   factory User.fromJson(Map<String, dynamic> json) {
-
     return User(
-
       email: json['email'],
-
       state: json['state'],
-
       name: json['name'],
-
       password: json['password'],
-
     );
-
   }
-
 }
-
-
 
 class SignupPage extends StatefulWidget {
-
   @override
-
   State<SignupPage> createState() => _SignupPageState();
-
 }
 
-
-
 class _SignupPageState extends State<SignupPage> {
-
   final TextEditingController _emailcontroller = TextEditingController();
 
   final TextEditingController _namecontroller = TextEditingController();
@@ -231,8 +139,7 @@ class _SignupPageState extends State<SignupPage> {
   final TextEditingController _passwordcontroller = TextEditingController();
 
   final TextEditingController _confirmpasswordcontroller =
-
-  TextEditingController();
+      TextEditingController();
 
   String _statecontroller = 'housekeeper';
 
@@ -242,54 +149,31 @@ class _SignupPageState extends State<SignupPage> {
 
   final TextEditingController _agecontroller = TextEditingController();
 
-
-
   Future<User>? _futureUser;
 
   List<Step> getSteps(
-
-      _namecontroller,
-
-      _emailcontroller,
-
-      _statecontroller,
-
-      _phoneNumbercontroller,
-
-      _locationcontroller,
-
-      _passwordcontroller,
-
-      _confirmpasswordcontroller,
-
-      _agecontroller) =>
-
+          _namecontroller,
+          _emailcontroller,
+          _statecontroller,
+          _phoneNumbercontroller,
+          _locationcontroller,
+          _passwordcontroller,
+          _confirmpasswordcontroller,
+          _agecontroller) =>
       [
-
         Step(
-
             isActive: currentStep >= 0,
-
             title: Text("Step 1"),
-
             content: Column(
-
               children: <Widget>[
-
                 TextField(
-
                   controller: _namecontroller,
-
                   decoration: const InputDecoration(hintText: 'Full Name'),
-
                 ),
 
                 TextField(
-
                   controller: _emailcontroller,
-
                   decoration: const InputDecoration(hintText: 'Enter Email'),
-
                 ),
 
                 // TextField(
@@ -301,37 +185,24 @@ class _SignupPageState extends State<SignupPage> {
                 // ),
 
                 Row(
-
                   children: [
-
                     SizedBox(
-
                       width: MediaQuery.of(context).size.width * 0.8,
-
                       height: MediaQuery.of(context).size.height * 0.08,
-
                       child: DropdownButton(
-
                         // Initial Value
 
                         value: dropdownvalue,
 
                         iconEnabledColor: Colors.white,
 
-
-
                         // Array list of items
 
                         items: items.map((String items) {
-
                           return DropdownMenuItem(
-
                             value: items,
-
                             child: Text(items),
-
                           );
-
                         }).toList(),
 
                         // After selecting the desired option,it will
@@ -339,372 +210,190 @@ class _SignupPageState extends State<SignupPage> {
                         // change button value to selected value
 
                         onChanged: (String? newValue) {
-
                           _statecontroller = newValue;
-
                           setState(() {
-
                             dropdownvalue = newValue!;
-
                           });
-
                         },
-
                       ),
-
                     ),
-
                     Icon(Icons.keyboard_arrow_down)
-
                   ],
-
                 ),
-
               ],
-
             )),
-
         Step(
-
             isActive: currentStep >= 1,
-
             title: Text("Step 2"),
-
             content: Column(
-
               children: <Widget>[
-
                 TextField(
-
                   controller: _locationcontroller,
-
                   decoration: const InputDecoration(hintText: 'Enter Location'),
-
                 ),
-
                 TextField(
-
                   controller: _phoneNumbercontroller,
-
                   decoration:
-
-                  const InputDecoration(hintText: 'Enter phoneNumber'),
-
+                      const InputDecoration(hintText: 'Enter phoneNumber'),
                 ),
-
                 TextField(
-
                   controller: _agecontroller,
-
                   decoration: const InputDecoration(hintText: 'Enter age'),
-
                 ),
-
               ],
-
             )),
-
         Step(
-
             isActive: currentStep >= 2,
-
             title: Text("Step 3"),
-
             content: Column(
-
               children: <Widget>[
-
                 TextField(
-
                   controller: _passwordcontroller,
-
                   decoration: const InputDecoration(hintText: 'Enter Password'),
-
                 ),
-
                 TextField(
-
                   controller: _confirmpasswordcontroller,
-
                   decoration:
-
-                  const InputDecoration(hintText: 'Confirm Password'),
-
+                      const InputDecoration(hintText: 'Confirm Password'),
                 ),
-
               ],
-
             )),
-
       ];
-
-
 
 // we will be creating a widget for text field
 
   Widget inputFile({
-
     label,
-
     obscureText = false,
-
   }) {
-
     return Column(
-
       crossAxisAlignment: CrossAxisAlignment.start,
-
       children: <Widget>[
-
         Text(
-
           label,
-
           style: TextStyle(
-
               fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
-
         ),
-
         SizedBox(
-
           height: 5,
-
         ),
-
         TextField(
-
           obscureText: obscureText,
-
           decoration: InputDecoration(
-
               filled: true,
-
               contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-
               enabledBorder: OutlineInputBorder(
-
                 borderSide: BorderSide(),
-
                 borderRadius: BorderRadius.circular(50),
-
               ),
-
               border: OutlineInputBorder(
-
                 borderSide: BorderSide(),
-
                 borderRadius: BorderRadius.circular(50),
-
               )),
-
         ),
-
         SizedBox(
-
           height: 10,
-
         )
-
       ],
-
     );
-
   }
 
-
-
   @override
-
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       resizeToAvoidBottomInset: false,
-
       backgroundColor: Colors.white,
-
       appBar: AppBar(
-
         title: Text("Signup"),
-
         centerTitle: true,
-
         elevation: 0,
-
         brightness: Brightness.light,
-
         backgroundColor: Color(0xff0054FF),
-
         leading: IconButton(
-
           onPressed: () {
-
             Navigator.pop(context);
-
           },
-
           icon: Icon(
-
             Icons.arrow_back_ios,
-
             size: 20,
-
             color: Colors.white,
-
           ),
-
         ),
-
       ),
-
       body: Column(
-
         children: [
-
           Expanded(
-
             child: Theme(
-
               data: Theme.of(context).copyWith(
-
                   colorScheme: ColorScheme.light(primary: Color(0xff0054FF))),
-
               child: Stepper(
-
                 controlsBuilder: ((context, details) {
-
                   return Container(
-
                     margin: EdgeInsets.only(top: 50),
-
                     child: Row(
-
                       children: [
-
                         Expanded(
-
                             child: ElevatedButton(
-
-                              child: Text("Next"),
-
-                              onPressed: details.onStepContinue,
-
-                            )),
-
+                          child: Text("Next"),
+                          onPressed: details.onStepContinue,
+                        )),
                         const SizedBox(width: 12),
-
                         if (currentStep != 0)
-
                           Expanded(
-
                               child: ElevatedButton(
-
-                                child: Text("Cancel"),
-
-                                onPressed: details.onStepCancel,
-
-                              )),
-
+                            child: Text("Cancel"),
+                            onPressed: details.onStepCancel,
+                          )),
                       ],
-
                     ),
-
                   );
-
                 }),
-
                 type: StepperType.horizontal,
-
                 steps: getSteps(
-
                     _namecontroller,
-
                     _emailcontroller,
-
                     _statecontroller,
-
                     _phoneNumbercontroller,
-
                     _locationcontroller,
-
                     _passwordcontroller,
-
                     _confirmpasswordcontroller,
-
                     _agecontroller),
-
                 currentStep: currentStep,
-
                 onStepContinue: () {
-
                   setState(() {
-
                     if (currentStep != 2) {
-
                       currentStep += 1;
-
                     } else {
-
                       globals.email = _emailcontroller.text;
 
                       //send Data!
 
                       _futureUser = createUser(
-
                           context,
-
                           _namecontroller.text,
-
                           _emailcontroller.text,
-
                           _statecontroller,
-
                           _phoneNumbercontroller.text,
-
                           _locationcontroller.text,
-
                           _passwordcontroller.text,
-
                           _confirmpasswordcontroller.text,
-
                           _agecontroller.text);
-
                     }
-
                   });
-
                 },
-
                 onStepCancel: () {
-
                   setState(() {
-
                     if (currentStep != 0) {
-
                       currentStep -= 1;
-
                     }
-
                   });
-
                 },
-
               ),
-
             ),
-
           ),
-
           SizedBox(height: 10),
-
         ],
-
       ),
-
     );
-
   }
-
 }
-
